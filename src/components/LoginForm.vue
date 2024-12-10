a923f2a5e3db97b404e81f2dc5a1556dcaf70e15
nknd1 <prodblood@gmail.com>
2024-12-10 22:13:59 +0500

    add toastify

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -13,6 +13,7 @@
 <script lang="ts">
 import ButtonComponent from '@/components/ButtonComponent.vue'
 import { toast } from 'vue3-toastify';
+import "vue3-toastify/dist/index.css";
 
 export default {
   components: {
@@ -28,31 +29,29 @@ export default {
       
       this.isLoading = true;
 
-      await new Promise((resolve) => setTimeout(resolve, 10000));
-
-      alert("hi")
+      await new Promise((resolve) => setTimeout(resolve, 10));
 
       this.isLoading = false;
 
-
-      const notify = () => {
-      toast("Wow so easy !", {
-        autoClose: 1000,
-      }); // ToastOptions
-    }
-    return { notify };
-   }
-    
-  },
-};
+    toast("пажжи!", {  
+      "theme": "auto",
+  "type": "success",
+  "position": "bottom-right",
+  "transition": "flip",
+  "dangerouslyHTMLString": true
+})
+    } 
+  }
+}
 </script>
 
 <style scoped lang="sass">
 
 input
-  @apply w-full mx-0 my-2.5 border-solid text-2xl p-3.5 text-black placeholder-gray-400 transition duration-100 ease-in-out bg-white border border-gray-300 rounded shadow-sm focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed
+  @apply w-full mx-0 my-2.5 border-solid text-2xl p-2 text-black placeholder-gray-400 transition duration-100 ease-in-out bg-white border border-gray-300 rounded shadow-sm focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed
 
 button
   @apply cursor-pointer w-full py-3.5 my-5 border-[none] bg-[#008fcf]
+
 
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   css: {
@@ -11,6 +12,7 @@ export default defineConfig({
       sass: {
         api: 'modern',
       }
+
     }
   },
   plugins: [
